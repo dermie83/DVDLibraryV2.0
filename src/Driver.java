f36151ddcc3a38883243726069c22b70638c034a
dermie83 <dermiemadsen@gmail.com>
2023-04-25 16:54:15 +0100

    Delete and Update Method added

--- src/Driver.java
+++ src/Driver.java
@@ -7,6 +7,7 @@ public class Driver {
 
     public Driver(){
         library = new Library();
+
     }
 
     private void addDVD(){
@@ -16,6 +17,45 @@ public class Driver {
         String dvdTitle = input.nextLine();
 
         library.add(new DVD(dvdTitle));
+        System.out.println("New DVD added to library.");
+
+    }
+
+    private void updateDVD(){
+        System.out.println(library.listDVDs());
+        if (library.getDVDs().size() > 0)
+            System.out.print("Enter the index of the DVD to edit >>>:  ");
+            int index = input.nextInt();
+            if((index >= 0) && (index <= library.getDVDs().size())) {
+
+            // gather new details of DVD by user
+            input.nextLine();
+            System.out.print("Enter the new DVD title:  ");
+            String dvdTitle = input.nextLine();
+
+            DVD dvd = library.getDVDs().get(index);
+            dvd.setDVDTitle(dvdTitle);
+
+        }
+        else {
+            System.out.println("There is no DVD for this index number");
+        }
+    }
+
+    private void deleteDVD(){
+        // list the DVDs and ask the user to delete the DVD from the array list
+        System.out.println(library.listDVDs());
+        if (library.getDVDs().size() > 0){
+            System.out.print("Enter the index of the DVD to delete >>>:  ");
+            int index = input.nextInt();
+            if((index >= 0) && (index <= library.getDVDs().size())){
+                library.getDVDs().remove(index);
+                System.out.println("DVD deleted!");
+            }
+            else{
+                System.out.println("There is no DVD for this index number");
+            }
+        }
 
     }
 
@@ -24,6 +64,8 @@ public class Driver {
         System.out.println("---------");
         System.out.println("  1) Add a DVD to library");
         System.out.println("  2) List all DVDs in library");
+        System.out.println("  3) Update a DVD in library");
+        System.out.println("  4) Delete a DVD from library");
         System.out.println("  0) Exit");
         System.out.print("==>> ");
         int option = input.nextInt();
@@ -42,6 +84,12 @@ public class Driver {
                     break;
                 case 2:
                     System.out.println(library.listDVDs());
+                    break;
+                case 3:
+                    updateDVD();
+                    break;
+                case 4:
+                    deleteDVD();
                     break;
                 default:
                     System.out.println("Invalid option selected.");
